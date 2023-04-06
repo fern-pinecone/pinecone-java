cb4207e55d0e5514ac17748e6b24ba4556020b26
dsinghvi <deep@usebirch.com>
2023-04-05 19:50:27 -0700

    Revert "Revert "update readme and sample app""
    This reverts commit 4d0910de65a97cc9d89d83a44b1ad4b7803d18be.

--- sample-app/src/main/java/sample/App.java
+++ sample-app/src/main/java/sample/App.java
@@ -1,9 +1,23 @@
 package sample;
 
+import com.pinecone.api.resources.types.CreateRequest;
 import java.lang.String;
+import com.pinecone.api.PineconeApiClient;
+import java.util.List;
 
 public final class App {
   public static void main(String[] args) {
-    // import com.pinecone.api.PineconeApiClient
+    PineconeApiClient pinecone = PineconeApiClient.builder()
+            .apiKey(System.getenv("PINECONE_API_KEY"))
+            .environment(System.getenv("us-west1-gcp"))
+            .build();
+
+    pinecone.createIndex(CreateRequest.builder()
+            .name("example-index")
+            .dimension(1024)
+            .build());
+
+    List<String> activeIndexes = pinecone.listIndexes();
+    System.out.println("Received active indexes from pinecone: " + activeIndexes);
   }
 }
